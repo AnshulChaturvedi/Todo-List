d9fbfa6975298c86d4335c102ce43df1530cdbe1
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-02-02 11:28:16 +0530

    getTodoById Added

--- Backend/controllers/getTodo.js
+++ Backend/controllers/getTodo.js
@@ -22,3 +22,34 @@ exports.getTodo = async(req,res) => {
         });
     }
 }
+
+exports.getTodoById = async(req,res) => {
+    try{
+        const id = req.params.id;
+        const todo = await Todo.findById({_id:id});
+
+        if(!todo){
+            return res.status(404)
+            .json({
+                success:false,
+                message:"No Data Found By the Given ID",
+            });
+        }
+        res.status(200)
+        .json({
+            success:true,
+            data:todo,
+            message:`Data for the given ID ${id} is found`,
+        });
+    }
+    catch(err){
+        console.error(err),
+        console.log(err),
+        res.status(500)
+        .json({
+            success:false,
+            error:err.message,
+            message:"Error in the Server",
+        });
+    }
+}
